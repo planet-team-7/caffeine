8ab7ba1c9b271894a4f4afea3fca9d5c05e51c9e
Kyoungsik Choi <kyoungsik.dev@gmail.com>
2018-09-01 16:49:07 +0900

    Chrome Extenstion Basic

---
+++ src/inject.js
@@ -0,0 +1,8 @@
+(function() {
+    var div = document.createElement('div');
+    div.style.position = 'fixed';
+    div.style.top = 0;
+    div.style.right = 0;
+    div.textContent = 'hello, world';
+    document.body.appendChild(div);
+})();
