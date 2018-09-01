b1641c24be064034624d3934f3669d3e4a805ea2
Kyoungsik Choi <kyoungsik.dev@gmail.com>
2018-09-02 01:50:34 +0900

    fix

--- src/animation.js
+++ src/animation.js
@@ -1,17 +1,23 @@
 onload = function startAnimation() { 
 
+
     // init
-    let characterSpeed = 50;
+    let bugSpeed = 50;
+    let developerSpeed = 100;
     let bgSpeed = 10;
     
 
     document.getElementById('slow').onclick = function(){
-        characterSpeed += 10;
+        bugSpeed += 20;
+        developerSpeed += 10;
         bgSpeed += 2;
     };
     document.getElementById('fast').onclick = function(){
-        if (characterSpeed > 10) {
-            characterSpeed -= 10;
+        if (bugSpeed > 20) {
+            bugSpeed -= 20;
+        }
+        if (developerSpeed > 10) {
+            developerSpeed -= 10;
         }
         if (bgSpeed > 2) {
             bgSpeed -= 2;
@@ -24,21 +30,14 @@ onload = function startAnimation() {
     var developerFrames = document.getElementById("developer").children;
     var developerFrameCount = developerFrames.length;
 
-    var i = 0;
-    var j = 0;
-
-    
+    var bugCnt = 0;
+    var developerCnt = 0;
     
     var bg1_left = 0;
     var bg2_left = 100;
 
-    startLoop = function() {
-        bugFrames[i % bugFrameCount].style.display = "none";
-        bugFrames[++i % bugFrameCount].style.display = "block";
 
-        developerFrames[j % developerFrameCount].style.display = "none";
-        developerFrames[++j % developerFrameCount].style.display = "block";
-
+    backgroundLoop = function() {
         bg1_left--;
         bg2_left--;
         if (bg1_left <= -100) {
@@ -49,8 +48,23 @@ onload = function startAnimation() {
         }
         document.getElementById('bg1').style.left = bg1_left + "%";
         document.getElementById('bg2').style.left = bg2_left + "%";
-        
-        window.setTimeout(startLoop, characterSpeed);
+
+        window.setTimeout(backgroundLoop, bugSpeed);
     }
-    startLoop();
+    bugLoop = function() {
+        bugFrames[bugCnt % bugFrameCount].style.display = "none";
+        bugFrames[++bugCnt % bugFrameCount].style.display = "block";
+
+        window.setTimeout(bugLoop, bugSpeed);
+    }
+    developerLoop = function() {
+        developerFrames[developerCnt % developerFrameCount].style.display = "none";
+        developerFrames[++developerCnt % developerFrameCount].style.display = "block";
+
+        window.setTimeout(developerLoop, developerSpeed);
+    }
+
+    bugLoop();
+    developerLoop();
+    backgroundLoop();
 } 
--- src/contentScript.js
+++ src/contentScript.js
@@ -11,13 +11,41 @@ inputArray.forEach(function(input){
     input.setAttribute('onkeydown', 'detectKeyDown();');
 });
 
-var iFrame  = document.createElement ("iframe");
-iFrame.src  = chrome.extension.getURL ("animation.html");
+const htmldata = `
+    <div class="button">
+            <button id="slow">느리게</button>
+            <button id="fast">빠르게</button>
+        </div>
+        <div id="playground">
+            <div id="bgArea">
+                <img class="bgImg" id="bg1" src="img/background.png" />
+                <img class="bgImg" id="bg2" src="img/background.png" />
+            </div>
+            <div id="bug"> 
+                <img class="img1" src="img/bug_1.png" />
+                <img class="img1" src="img/bug_2.png" />
+                <img class="img1" src="img/bug_3.png" />
+                <img class="img1" src="img/bug_4.png" />
+            </div>
+            <div id="developer"> 
+                <img class="img1" src="img/developer_1.png" />
+                <img class="img1" src="img/developer_2.png" />
+                <img class="img1" src="img/developer_3.png" />
+                <img class="img1" src="img/developer_4.png" />
+            </div>
+        </div>
+    `;
 
-iFrame.style.width = '100%';
-iFrame.style.height = '300px';
-iFrame.style.position = 'fixed';
-iFrame.style.bottom = '0';
-iFrame.style.zIndex = '100';
+    var e  = document.createElement ("div");
+    e.innerHTML = htmldata;
 
-document.body.insertBefore (iFrame, document.body.lastChild);
+
+e.style.width = '100%';
+e.style.height = '300px';
+e.style.position = 'fixed';
+e.style.bottom = '0';
+e.style.zIndex = '100';
+
+//document.body.insertBefore (iFrame, document.body.lastChild);
+console.log(e);
+document.body.appendChild(e);
